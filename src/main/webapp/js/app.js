7d01dc42fd3c2d23ef4b4a2657b8a815018f17c0
TARTARUSPC\TartaPc <pmasch@hm.edu>
2017-04-27 07:04:51 +0200

    Letzten Änderungen von Gestern...

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,1 +1,10 @@
-var shareit = angular.module('shareit',[]);
+'use strict';
+
+var shareit = angular.module('shareit',['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider.when('/newBook',
+            {
+                templateUrl:'templates/NewBook.html',
+                controller: 'EditBookController'
+            })
+    });
