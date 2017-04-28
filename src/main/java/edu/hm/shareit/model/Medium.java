eb4f63e0ab397930be07f265f99ab14f102b8d11
Martin Doerner <martin.doerner@yahoo.de>
2017-04-28 13:40:30 +0200

    model/Medium tests, added null-check in Medium constructor, removed nullcheck in getTitle method as it is not possible to set a null value for a title now.

--- src/main/java/edu/hm/shareit/model/Medium.java
+++ src/main/java/edu/hm/shareit/model/Medium.java
@@ -1,8 +1,5 @@
 package edu.hm.shareit.model;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
 /**
  *
  */
@@ -29,12 +26,12 @@ public class Medium {
 
         Medium medium = (Medium) o;
 
-        return title != null ? title.equals(medium.title) : medium.title == null;
+        return title.equals(medium.title);
     }
 
     @Override
     public int hashCode() {
-        return title != null ? title.hashCode() : 0;
+        return title.hashCode();
     }
 
     @Override
--- src/test/edu/hm/shareit/model/MediumTest.java
+++ src/test/edu/hm/shareit/model/MediumTest.java
@@ -2,15 +2,54 @@ package edu.hm.shareit.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MediumTest {
-    
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void simpleTest(){
+    public void simpleTest() {
         Medium m = new Medium("BeispielMedium");
         assertEquals("Test ob Titel gespeichert wurde", m.getTitle(), "BeispielMedium");
     }
-    
-    
+
+    @Test
+    public void nullMediumTitleTest() {
+        exception.expect(IllegalArgumentException.class);
+        Medium m = new Medium(null);
+    }
+
+    @Test
+    public void equalsTest() {
+        Medium root = new Medium("root");
+        assertFalse(root.equals(new Object()));
+        assertFalse(root.equals(5));
+        assertFalse(root.equals("bamboo"));
+        assertFalse(root.equals(new Medium("otherMedium")));
+        assertFalse(root.equals(false));
+        assertFalse(root.equals(null));
+        assertTrue(root.equals(new Medium("root")));
+        assertTrue(root.equals(root));
+    }
+
+    @Test
+    public void hashTest() {
+        Medium m1 = new Medium("Medium");
+        Medium m2 = new Medium("Medium");
+        Medium m3 = new Medium("Another");
+        assertEquals(m1.hashCode(), m2.hashCode());
+        assertNotEquals(m1.hashCode(), m3.hashCode());
+        assertNotEquals(m2.hashCode(), m3.hashCode());
+    }
+
+    @Test
+    public void toStringTest() {
+        Medium m = new Medium("MediumUnderTest");
+        assertEquals(m.toString(), "Medium{title='MediumUnderTest'}");
+    }
+
 }
