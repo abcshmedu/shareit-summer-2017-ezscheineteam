2193350d1f215a5c51638b99027fb9912cc30f8e
Martin Doerner <zubdrajok@o2.pl>
2017-05-04 23:37:53 +0200

    BookTest finished

--- src/test/java/edu/hm/shareit/model/BookTest.java
+++ src/test/java/edu/hm/shareit/model/BookTest.java
@@ -11,9 +11,9 @@ import java.io.IOException;
 
 public class BookTest {
     public final ExpectedException exception = ExpectedException.none();
-    
+
     static String jsonInput = "{\"title\":\"Buch\", \"author\": \"Author\", \"isbn\": \"1234\"}";
-    
+
     @Test
     public void serialize() throws JsonProcessingException {
         ObjectMapper m = new ObjectMapper();
@@ -23,6 +23,7 @@ public class BookTest {
         String result = m.writeValueAsString(b);
         assertEquals(toParse, result);
     }
+
     @Test
     public void deserialize1() throws IOException {
         ObjectMapper m = new ObjectMapper();
@@ -32,34 +33,58 @@ public class BookTest {
         assertEquals("Author", b.getAuthor());
         assertEquals("1234", b.getIsbn());
     }
-    
+
     @Test
-    public void emptyBookEqualsNullTest(){
+    public void emptyBookEqualsNullTest() {
         Book b = new Book();
         assertFalse(b.equals(null));
     }
-    
+
     @Test
-    public void BookWithAllNullParams(){
+    public void BookWithAllNullParams() {
         exception.expect(IllegalArgumentException.class);
         Book b = new Book(null, null, null);
     }
-    
+
     @Test
-    public void BookWithNullParamsEqualsTest(){
+    public void BookWithNullParamsEqualsTest() {
         Book bookAuthorNull = new Book("LENO PALENO", null, "1234567890123");
         Book bookISBNNull = new Book("LENO PALENO", "PETER", null);
         assertFalse(bookAuthorNull.equals(bookISBNNull));
         assertFalse(bookISBNNull.equals(bookAuthorNull));
     }
-    
+
     @Test
-    public void BooksWithDifferentParamsEqualsTest(){
+    public void BooksWithDifferentParamsEqualsTest() {
         Book bookOne = new Book("LENO PALENO", "PETER", "1234567890123");
         Book bookTwo = new Book("LENO PALENO", "PETER", "1234567");
         assertFalse(bookOne.equals(bookTwo));
     }
 
+    @Test
+    public void BookWithNullParamsHashTest() {
+        Book b = new Book("LENO PALENO", null, null);
+        int expected = "LENO PALENO".hashCode() * 31 * 31;
+        assertEquals(expected, b.hashCode());
 
+        b = new Book("LENO PALENO", "author", null);
+        expected = ("LENO PALENO".hashCode() * 31 + "author".hashCode()) * 31;
+        assertEquals(expected, b.hashCode());
+
+        b = new Book("LENO PALENO", null, "");
+        expected = "LENO PALENO".hashCode() * 31 * 31 + "".hashCode();
+        assertEquals(expected, b.hashCode());
+    }
     
+    @Test
+    public void ToStringTest(){
+        Book b = new Book("LENO PALENO", "PETER", "1234567890123");
+        String expected = "{"
+                + "title='" + "LENO PALENO" + '\''
+                + ", author='" + "PETER" + '\''
+                + ", isbn='" + "1234567890123" + '\''
+                + '}';
+        assertEquals(expected, b.toString());
+    }
+
 }
