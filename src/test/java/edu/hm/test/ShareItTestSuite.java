27a4296d74038fd9faa2410a3dad4c33fbd4a615
Martin Doerner <mdoerner@hm.edu>
2017-05-04 10:39:17 +0200

    TestSuite extended, added other test classes

--- src/test/java/edu/hm/test/ShareItTestSuite.java
+++ src/test/java/edu/hm/test/ShareItTestSuite.java
@@ -3,10 +3,13 @@ package edu.hm.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import edu.hm.shareit.client.MediaClientTest;
 import edu.hm.shareit.model.BookTest;
 import edu.hm.shareit.model.CopyTest;
 import edu.hm.shareit.model.DiscTest;
 import edu.hm.shareit.model.MediumTest;
+import edu.hm.shareit.resource.*;
+import edu.hm.shareit.util.*;
 
 @RunWith(Suite.class)
 
@@ -14,7 +17,11 @@ import edu.hm.shareit.model.MediumTest;
    BookTest.class,
    CopyTest.class,
    DiscTest.class,
-   MediumTest.class
+   MediumTest.class,
+   MediaClientTest.class,
+   CopyResourceTest.class,
+   MediaResourceTest.class,
+   MediumUtilTest.class
 })
 
 public class ShareItTestSuite {}
