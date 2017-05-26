4ad3956fb1f280aa565b769d3bf91af0a1aa6065
Mashmallow <pmasch@hm.edu>
2017-05-26 14:05:38 +0200

    Add exception handling

---
+++ src/main/java/edu/hm/shareit/util/JSONParseExceptionMapper.java
@@ -0,0 +1,22 @@
+package edu.hm.shareit.util;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import edu.hm.shareit.business.ServiceStatus;
+
+import javax.inject.Singleton;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Maps all Exceptions occurring during json parsing to an appropriate server response.
+ */
+@Provider
+@Singleton
+public class JSONParseExceptionMapper implements ExceptionMapper<JsonParseException> {
+    @Override
+    public Response toResponse(JsonParseException e) {
+        return Response.status(ServiceStatus.ERROR_PARSING_JSON.getStatus()).entity(ServiceStatus.ERROR_PARSING_JSON).build();
+    }
+}
+
